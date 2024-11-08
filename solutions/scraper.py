e3494f74864daf884469c1d19d03e76d9fc39f97
M-Zubair10 <imuhammadzubair223@gmail.com>
2024-11-08 15:26:37 +0500

    initial commit

--- solutions/scraper.py
+++ solutions/scraper.py
@@ -40,12 +40,12 @@ class Scraper(Selenium):
     def handle_captcha(self):
         slider = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="verify-move-block"]')))
         self.actions.click_and_hold(slider).perform()
-        for i in range(60):
-            self.actions.move_by_offset(5, 0).perform()
+        for i in range(120):
+            self.actions.move_by_offset(2, 0).perform()
             if self.detect():
                 self.actions.release(slider).perform()
                 break
-            time.sleep(0.2)
+            time.sleep(0.1)
         input(">>>")
         return True
 
