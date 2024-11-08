b06e1bb04aeeee20b8015bad219f023a8789c930
M-Zubair10 <imuhammadzubair223@gmail.com>
2024-11-08 15:20:30 +0500

    initial commit

--- solutions/support/model/model.py
+++ solutions/support/model/model.py
@@ -32,7 +32,7 @@ class ONNXModel:
         if not isinstance(input_data, np.ndarray):
             input_data = np.array(input_data)
         outputs = self.session.run(None, {self.input_name: input_data})
-        return not np.argmax(outputs[0][0])
+        return not np.argmax(outputs[0][0]) and np.max(outputs[0][0]) > 0.9
 
     @staticmethod
     def preprocess_image(image, target_size=(64, 64)):
